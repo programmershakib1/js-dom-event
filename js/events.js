d01a5ed9f6584b1ec5dcc3efbae52019b1b15637
programmershakib <programmershakibinfo@gmail.com>
2024-09-19 00:32:00 +0600

    first commit

---
+++ js/events.js
@@ -0,0 +1,44 @@
+// option: 1 directly set on the HTML element
+// <button onclick="document.body.style.backgroundColor='yellow'">Make Yellow</button>
+
+// option: 2 add onclick function on the html element
+// IMPORTANT: we will use this
+// <button onclick="makeRed()">Make Red</button>
+function makeRed() {
+  document.body.style.backgroundColor = "red";
+}
+
+// option: 3
+const makeBlueButton = document.getElementById("make-blue");
+makeBlueButton.onclick = makeBlue;
+
+function makeBlue() {
+  document.body.style.backgroundColor = "blue";
+}
+
+// option: 3 another
+const purpleButton = document.getElementById("make-purple");
+purpleButton.onclick = function makePurple() {
+  document.body.style.backgroundColor = "purple";
+};
+
+// option: 4
+const pinkButton = document.getElementById("make-pink");
+pinkButton.addEventListener("click", makePink);
+
+function makePink() {
+  document.body.style.backgroundColor = "pink";
+}
+
+// option: 4 another
+const makeGreenButton = document.getElementById("make-green");
+makeGreenButton.addEventListener("click", function makeGreen() {
+  document.body.style.backgroundColor = "green";
+})
+
+// option: 4 Final
+// IMPORTANT: we will use this sometimes
+// document.getElementById('make-goldenrod').addEventListener('click', function(){})
+document.getElementById('make-goldenrod').addEventListener('click', function(){
+    document.body.style.backgroundColor = 'goldenrod';
+})
